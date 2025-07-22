33df508a7e4d480914bb78bafeba89e47a0aebbf
gantulu <117321632+gantulu@users.noreply.github.com>
2025-07-22 23:22:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,20 @@
-  async function fetchTopCoins() {
-    try {
-      const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=idr&order=volume_desc&per_page=12&page=1&sparkline=false');
-      const coins = await response.json();
-
-      const container = document.getElementById('crypto-list');
-      container.innerHTML = '';
-
-      coins.forEach((coin, index) => {
-        const item = document.createElement('div');
-        item.className = 'crypto-item';
-        setTimeout(() => item.classList.add('show'), index * 100); // animasi delay
-
-        item.innerHTML = `
-          <div class="crypto-left">
-            <img src="${coin.image}" alt="${coin.name}" class="crypto-logo">
-            <div>
-              <div class="crypto-name">${coin.symbol.toUpperCase()} - ${coin.name}</div>
-            </div>
-          </div>
-          <div class="crypto-price">
-            <div>Rp ${coin.current_price.toLocaleString('id-ID')}</div>
-            <div class="${coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'}">
-              ${coin.price_change_percentage_24h.toFixed(2)}%
-            </div>
-          </div>
-        `;
-
-        container.appendChild(item);
-      });
-
-    } catch (error) {
-      console.error('Gagal ambil data:', error);
-      document.getElementById('crypto-list').innerHTML = '<p>Gagal memuat data.</p>';
+async function callEndpoint(img1) {
+    const url = 'https://qlpav7.buildship.run/untitledFlow-2b2016a16bb1';
+    const data = {
+        img1: img1
     }
-  }
-
-  fetchTopCoins();
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        });
+        const result = await response.json();
+        console.log('Success:', result);
+        return result;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
