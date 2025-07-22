70887c3fb900eae6a8594bfe4af6395c42334413
gantulu <117321632+gantulu@users.noreply.github.com>
2025-07-23 01:03:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const fileInput = document.getElementById('img1');
+const img1Input = document.getElementById('img1');
+const uploadBtn = document.getElementById('upload-btn');
 
-fileInput.addEventListener('change', (e) => {
-  const file = fileInput.files[0];
-  console.log(file);
+uploadBtn.addEventListener('click', () => {
+  const file = img1Input.files[0];
+  if (file) {
+    const formData = new FormData();
+    formData.append('img1', file);
+
+    fetch('https://qlpav7.buildship.run/file-uploadsf', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  } else {
+    console.error('Tidak ada file yang dipilih');
+  }
 });
