e6950d49d6572976cb071d4e675c2de60d6248e1
gantulu <117321632+gantulu@users.noreply.github.com>
2025-10-19 07:16:43 +0700

    Add files via upload

---
+++ bur/sw.js
@@ -0,0 +1,14 @@
+const cacheName = 'data-list-card-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/notif.mp3',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => { e.waitUntil(caches.open(cacheName).then(c=>c.addAll(assetsToCache))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
