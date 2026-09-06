d8d667744835913d5456959f4a90f3c1a6a3551c
gantulu <117321632+gantulu@users.noreply.github.com>
2026-09-07 04:39:33 +0800

    Clear repository contents

--- coldiv.js
+++
@@ -1,9 +0,0 @@
-function showDiv(selectedId) {
-  const allDivs = document.querySelectorAll('.color-div');
-  allDivs.forEach(div => div.style.display = 'none');
-
-  const targetDiv = document.getElementById(selectedId + 'Div');
-  if (targetDiv) {
-    targetDiv.style.display = 'block';
-  }
-}
--- grapedrop.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-  const badge = document.querySelector('.page-brand-badge');
-  if (badge) {
-    badge.style.display = 'none';
-  }
-};
